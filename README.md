1e23e78bf554850012fec388dae0355b36e656b7
Fabian Schlieper <fabian@schlieper.email>
2017-10-25 16:05:28 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+This is a fork from (HelloAndroidCamera2)[https://github.com/halide/Halide/tree/master/apps/HelloAndroidCamera2].
+
+It uses Android Camera2 API to capture a YUV stream, applies an edge detector and draws the result directly on the Java SurfaceView through ANativeWindow.
+
+
+This fork is compatible with latest Android Studio C++ CMake toolchain.
+
+# Setup
+
+In `gradle.properties` set
+```
+halideDir=A:/dev/cv/halide-build
+hostClang=C:/data-ssd/dev/cv/llvm-install/bin/clang++.exe
+```
+`halideDir` points to your Halide release build (either CMake or make, in-tree or out-of-tree).
+
+`hostClang` points to the clang executable.
+
+
+# Linking Halide Pipelines
+`app/src/main/cpp/hl-pipelines/CMakeLists.txt` generates and links the Halide pipelines to the native JNI library.
+
+It expects all `*.cpp` files in the same folder to be generators with a call to `HALIDE_REGISTER_GENERATOR(...)`.
+
+
+# Supported Architectures
+* arm-64
---
+++ app/src/main/java/com/example/halideandroidcamera2gl/PermissionConfirmationDialog.java
@@ -0,0 +1,42 @@
+package com.example.halideandroidcamera2gl;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v13.app.FragmentCompat;
+
+/**
+ * A dialog that explains about the necessary permissions.
+ */
+public class PermissionConfirmationDialog extends DialogFragment {
+
+    public static PermissionConfirmationDialog newInstance() {
+        return new PermissionConfirmationDialog();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Fragment parent = getParentFragment();
+        return new AlertDialog.Builder(getActivity())
+                .setMessage(R.string.request_permission)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FragmentCompat.requestPermissions(parent, Camera2BasicFragment.CAMERA_PERMISSIONS,
+                                Camera2BasicFragment .REQUEST_CAMERA_PERMISSIONS);
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                getActivity().finish();
+                            }
+                        })
+                .create();
+    }
+
+}
